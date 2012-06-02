fd55e21169efaae03272cc0a4bcf51f1f9250a60
Andy Waite <github.aw@andywaite.com>
2012-06-02 14:55:52 +0100

    Adding tableview app template

--- lib/rubymotion_generators.rb
+++ lib/rubymotion_generators.rb
@@ -12,10 +12,6 @@ module RubymotionGenerators
       args = args.flatten
       template_name = args[0]
       name = args[1]
-      unless name
-        puts "Missing name parameter"
-        exit 1
-      end
       case template_name
       when 'view'
         generate_view name
@@ -29,6 +25,9 @@ module RubymotionGenerators
         generate_tab_bar_app_delegate name
         generate_view_controller 'first'
         generate_view_controller 'second'
+      when 'tvapp'
+        generate_table_view_app_delegate
+        generate_view_controller 'example'
       else
         puts "Invalid template name '#{template_name}'"
         exit 1
@@ -44,30 +43,35 @@ module RubymotionGenerators
       insert_into_file(output_path, class_name, :after => 'class ')
     end
 
-    def generate_table_view_controller(name)
+    def generate_table_view_controller(name = 'Untitled')
       output_path = "app/controllers/#{name}_table_view_controller.rb"
       template('templates/table_view_controller.rb', output_path)
       class_name = name.capitalize + "TableViewController"
       insert_into_file(output_path, class_name, :after => 'class ')
     end
 
-    def generate_view_controller(name)
+    def generate_view_controller(name = 'Untitled')
       output_path = "app/controllers/#{name}_view_controller.rb"
       template('templates/view_controller.rb', output_path)
       class_name = name.capitalize + "ViewController"
       insert_into_file(output_path, class_name, :after => 'class ')
     end
 
-    def generate_table_view_cell(name)
+    def generate_table_view_cell(name = 'Untitled')
       output_path = "app/views/cells/#{name}_table_view_cell.rb"
       template('templates/table_view_cell.rb', output_path)
       class_name = name.capitalize + "TableViewCell"
       insert_into_file(output_path, class_name, :after => 'class ')
     end
 
-    def generate_tab_bar_app_delegate(name)
+    def generate_tab_bar_app_delegate
       output_path = "app/app_delegate.rb"
       template('templates/tab_bar_app_delegate.rb', output_path)
+    end
+
+    def generate_table_view_app_delegate
+      output_path = "app/app_delegate.rb"
+      template('templates/table_view_app_delegate.rb', output_path)
     end
 
     def self.source_root
---
+++ lib/templates/table_view_app_delegate.rb
@@ -0,0 +1,53 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    tabbar = UITabBarController.alloc.init
+    table_view_controller = ExampleTableViewController.alloc.init
+    #tabbar.title = NSBundle.mainBundle.infoDictionary["CFBundleName"]
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(table_view_controller)
+    @window.makeKeyAndVisible
+    true
+  end
+
+  def applicationWillResignActive(application)
+    # Sent when the application is about to move from active to inactive state. This can occur for certain types of
+    # temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application
+    # and it begins the transition to the background state. Use this method to pause ongoing tasks, disable timers, and
+    # throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  end
+
+  def applicationDidEnterBackground(application)
+    # Use this method to release shared resources, save user data, invalidate timers, and store enough application
+    # state information to restore your application to its current state in case it is terminated later.If your
+    # application supports background execution, this method is called instead of applicationWillTerminate: when the
+    # user quits.
+  end
+
+  def applicationWillEnterForeground(application)
+    # Called as part of the transition from the background to the inactive state; here you can undo many of the
+    # changes made on entering the background.
+  end
+
+  def applicationDidBecomeActive(application)
+    # Restart any tasks that were paused (or not yet started) while the application was inactive. If the application
+    # was previously in the background, optionally refresh the user interface.
+  end
+
+  def applicationWillTerminate(application)
+    # Called when the application is about to terminate. Save data if appropriate. See also
+    # applicationDidEnterBackground
+  end
+
+=begin
+  # Optional UITabBarControllerDelegate method.
+  def tabBarController(tabBarController, didSelectViewController:viewController)
+  end
+=end
+
+=begin
+  # Optional UITabBarControllerDelegate method.
+  def tabBarController(tabBarController, didEndCustomizingViewControllers:viewControllers, changed:changed)
+  end
+=end
+end
+
