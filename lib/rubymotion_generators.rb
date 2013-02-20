3a175ff8654fe4dba9b48b4bb7aa63d74c8c7576
Andy Waite <github.aw@andywaite.com>
2013-02-20 15:04:07 +0000

    Assorted clean-up and bug fixes

--- lib/rubymotion_generators.rb
+++ lib/rubymotion_generators.rb
@@ -7,11 +7,9 @@ module RubymotionGenerators
 
     desc "generate", "Generate a new file of the specified type"
 
-    def generate(*args)
-      # unsure why this flatten step is needed
-      args = args.flatten
-      template_name = args[0]
-      name = args[1]
+    def generate(args)
+      template_name, name = args
+
       case template_name
       when 'view'
         generate_view name
@@ -20,7 +18,9 @@ module RubymotionGenerators
       when 'tvc'
         generate_table_view_controller name
       when 'cell'
-        generate_table_view_cell
+        generate_table_view_cell name
+      when 'app'
+        generate_basic_app
       when 'tbapp'
         generate_tab_bar_app_delegate
         generate_view_controller 'first'
@@ -40,46 +40,43 @@ module RubymotionGenerators
     private
 
     def generate_view(name)
-      output_path = "app/views/#{name}_view.rb"
-      template('templates/view.rb', output_path)
+      output_path = "app/views/#{name.downcase}_view.rb"
+      template 'templates/view.rb', output_path
       class_name = name.capitalize + "View"
-      insert_into_file(output_path, class_name, :after => 'class ')
+      insert_into_file output_path, class_name, :after => 'class '
     end
 
-    def generate_table_view_controller(name = 'Untitled')
-      output_path = "app/controllers/#{name}_table_view_controller.rb"
-      template('templates/table_view_controller.rb', output_path)
-      class_name = name.capitalize + "TableViewController"
-      insert_into_file(output_path, class_name, :after => 'class ')
-    end
-
-    def generate_view_controller(name = 'Untitled')
-      output_path = "app/controllers/#{name}_view_controller.rb"
-      template('templates/view_controller.rb', output_path)
+    def generate_view_controller(name)
+      output_path = "app/controllers/#{name.downcase}_view_controller.rb"
+      template 'templates/view_controller.rb', output_path
       class_name = name.capitalize + "ViewController"
-      insert_into_file(output_path, class_name, :after => 'class ')
+      insert_into_file output_path, class_name, :after => 'class '
     end
 
-    def generate_table_view_cell(name = 'Untitled')
-      output_path = "app/views/cells/#{name}_table_view_cell.rb"
-      template('templates/table_view_cell.rb', output_path)
-      class_name = name.capitalize + "TableViewCell"
-      insert_into_file(output_path, class_name, :after => 'class ')
+    def generate_table_view_controller(name)
+      output_path = "app/controllers/#{name.downcase}_table_view_controller.rb"
+      template 'templates/table_view_controller.rb', output_path
+      class_name = name.capitalize + "TableViewController"
+      insert_into_file output_path, class_name, :after => 'class '
+    end
+
+    def generate_table_view_cell(name)
+      output_path = "app/views/#{name.downcase}_cell.rb"
+      template 'templates/table_view_cell.rb', output_path
+      class_name = name.capitalize + "Cell"
+      insert_into_file output_path, class_name, :after => 'class '
     end
 
     def generate_tab_bar_app_delegate
-      output_path = "app/app_delegate.rb"
-      template('templates/tab_bar_app_delegate.rb', output_path)
+      template 'templates/tab_bar_app_delegate.rb', "app/app_delegate.rb"
     end
 
     def generate_table_view_app_delegate
-      output_path = "app/app_delegate.rb"
-      template('templates/table_view_app_delegate.rb', output_path)
+      template 'templates/table_view_app_delegate.rb', "app/app_delegate.rb"
     end
 
     def generate_single_view_app_delegate
-      output_path = "app/app_delegate.rb"
-      template('templates/single_view_app_delegate.rb', output_path)
+      template 'templates/single_view_app_delegate.rb', "app/app_delegate.rb"
     end
 
     def self.source_root
